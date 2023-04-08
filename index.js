8a5281662d2986977290a5d56167c3084bfb052b
Timothy Dam <vietdam89@gmail.com>
2023-04-08 16:01:44 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+// index.js
+
+const main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.appendChild(newHeader);
+
+const name = "Tim"; 
+newHeader.innerHTML = `${name} is the champion`;
+
